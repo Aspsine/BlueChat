44534de5b8474c30f17ff73afd54d91dde5e05bd
Aspsine <littleximail@gmail.com>
2015-02-04 16:16:34 +0800

    add ButterKnife framework.

--- app/src/main/java/com/aspsine/bluechat/App.java
+++ app/src/main/java/com/aspsine/bluechat/App.java
@@ -9,5 +9,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
     }
 }
--- app/src/main/java/com/aspsine/bluechat/ui/activity/ChatActivity.java
+++ app/src/main/java/com/aspsine/bluechat/ui/activity/ChatActivity.java
@@ -29,7 +29,6 @@ public class ChatActivity extends ActionBarActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
